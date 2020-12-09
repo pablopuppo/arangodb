27e309ac5b3518f7c48becb7a84aa9f98d373d90
Pablo Puppo <ilpuppo@gmail.com>
2020-12-09 11:50:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+const createRouter = require('@arangodb/foxx/router');
+const db = require('@arangodb').db;
+const { query } = require("@arangodb");
+const collectionName = 'ga';
+const router = createRouter();
+
+module.context.use(router);
+
+router.get('/ga-log', function (req, res) {
+    const run = query`
+        INSERT { "parms": "GET" , "time": DATE_NOW()} INTO 'gaLog'
+    `
+
+    res.send(run);
+  })
+  .response(['text/plain'], 'A generic greeting.')
+  .summary('Generic greeting')
+  .description('Prints a generic greeting.');
+
+  router.post('/ga-log', function (req, res) {
+    const run = query`
+        INSERT { "obj": ${req} , "time": DATE_NOW()} INTO 'gaLog'
+    `
+
+    res.send(run);
+  })
+  .response(['text/plain'], 'A generic greeting.')
+  .summary('Generic greeting')
+  .description('Prints a generic greeting.');
